494d6aaf409cbb99c560d43f0b02948fa62980db
asqguy496 <asqguy496@gmail.com>
2025-03-10 03:31:46 +0000

    Add 5.ts

---
+++ src/5.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+};
+
+export default App;
