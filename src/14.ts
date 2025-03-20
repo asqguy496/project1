37090943ebc67be0906cca82749ca0e342642491
asqguy496 <asqguy496@gmail.com>
2025-03-20 06:23:31 +0000

    Add 14.ts

---
+++ src/14.ts
@@ -0,0 +1,11 @@
+interface Person {
+  name: string;
+  age: number;
+}
+
+function createPerson(name: string, age: number): Person {
+  return { name, age };
+}
+
+const person = createPerson('John', 30);
+console.log(person);
