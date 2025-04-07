334d2d8ed5558850a9114722aed993e7d2dbcc26
asqguy496 <asqguy496@gmail.com>
2025-04-07 05:08:20 +0000

    Add 42.ts

---
+++ src/42.ts
@@ -0,0 +1,13 @@
+function getRandomString(length: number) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  
+  return result;
+}
+
+console.log(getRandomString(10)); // Example: 'P@#G9'
